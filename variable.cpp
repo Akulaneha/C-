faf9028d02839511dc8e67dfc7c97d99db82ea0a
Akulaneha <111947963+Akulaneha@users.noreply.github.com>
2023-03-02 11:03:27 +0530

    Add files via upload
    Signed-off-by: Akulaneha <111947963+Akulaneha@users.noreply.github.com>

---
+++ variable.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+int variable_int;
+int variable_float;
+char variable_char;
+cout<<"enter any integer number:";
+cin>>variable_int;
+cout<<"enter any floating pointnumber:";
+cin>> variable_float;
+cout<<"enter any character";
+cin>> variable_char;
+cout<<endl<<"integer"<<variable_int<<endl;
+cout<<endl<<"character"<< variable_char<<endl;
+cout<<endl<<"floating point"<<variable_float<<endl:;
+return 0;
+}
+
