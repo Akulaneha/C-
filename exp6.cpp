8e15f6d9a854e079015e0f062310b06e74cfab0b
Akulaneha <111947963+Akulaneha@users.noreply.github.com>
2023-03-02 10:40:22 +0530

    Add files via upload
    Signed-off-by: Akulaneha <111947963+Akulaneha@users.noreply.github.com>

---
+++ exp6.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+	char ch;
+	cout<<"\n enter character is lower case:";
+	ch = getchar();
+	cout<<"\n character in upper case:";
+	putchar(ch = 32);
+	return 0;
+}
